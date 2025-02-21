dbaca992d03f9430ea5da5b62ceae6b45eeeb559
Sergey Ortman <sergey@ortman.pro>
2025-02-21 18:01:25 +0500

    WinUSB firmware

--- CableTesterFirmware/USB_DEVICE/App/usb_device.c
+++ CableTesterFirmware/USB_DEVICE/App/usb_device.c
@@ -68,6 +68,14 @@ void MX_USB_DEVICE_Init(void)
   if (USBD_Init(&hUsbDeviceFS, &FS_Desc, DEVICE_FS) != USBD_OK) {
     Error_Handler();
   }
+  //if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_CDC) != USBD_OK)
+  //{
+  //  Error_Handler();
+  //}
+  //if (USBD_CDC_RegisterInterface(&hUsbDeviceFS, &USBD_Interface_fops_FS) != USBD_OK)
+  //{
+  //  Error_Handler();
+  //}
   if (USBD_RegisterClass(&hUsbDeviceFS, USBD_WINUSB_CLASS) != USBD_OK) {
     Error_Handler();
   }
